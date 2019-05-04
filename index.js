5cd8f2a9c541c798e795621d2bd7ec302b97c801
Rebecca <rrossman25@gmail.com>
2019-05-04 01:30:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(str){
 
 function sayHiToGrandma(str){
   if (str === str.toLowerCase()){
-    return "I can't hear you";
+    return "I can\'t hear you";
   }
   else if (str === str.toUpperCase){
-    
+    return "YES INDEED!"
   }
 }
